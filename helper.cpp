b37a1306912a6fe1565262d7ea834d237c494f9e
Jared Ma <jyma@sfu.ca>
2020-01-09 14:22:23 -0800

    seperate helper functions

---
+++ helper.cpp
@@ -0,0 +1,9 @@
+#include "helper.hpp"
+
+float toRadians(float deg) {
+    return deg * M_PI / 180;
+}
+
+float toDegrees(float rad) {
+    return rad * 180 / M_PI;
+}
---
+++ helper.hpp
@@ -0,0 +1,8 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+float toRadians(float deg);
+
+float toDegrees(float rad);
+
+#endif
